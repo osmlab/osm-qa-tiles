5fddd916b81f676516875093f53a06954e3e77a9
Jennings Anderson <jennings.anderson@gmail.com>
2020-07-21 14:03:53 -0700

    install dependencies in one script

---
+++ 0_install_dependencies.sh
@@ -0,0 +1,7 @@
+git clone --depth=1 https://github.com/mapbox/mason
+
+mason/mason install osmium-tool 1.11.0
+mason/mason link osmium-tool 1.11.0
+
+mason/mason install tippecanoe 1.32.10
+mason/mason link tippecanoe 1.32.10
