83d7eb6d53d8fe3f4b3847778584a4a614fe5fe2
Jennings Anderson <jennings.anderson@gmail.com>
2020-07-22 09:30:42 -0700

    Update 0_install_dependencies.sh

--- 0_install_dependencies.sh
+++ 0_install_dependencies.sh
@@ -5,3 +5,7 @@ mason/mason link osmium-tool 1.11.0
 
 mason/mason install tippecanoe 1.32.10
 mason/mason link tippecanoe 1.32.10
+
+#These are for filter-parallel if that script is getting used?
+npm install json-stream-reduce
+npm install @turf/area
